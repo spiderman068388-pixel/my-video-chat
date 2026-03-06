15755697401ebdfdbc46735bf661f75c77b7af48
spiderman068388-pixel <spiderman068388@gmail.com>
2026-03-06 14:54:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-app.use(express.static('./'));
-
-let waitingUser = null;
-
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    // Jab koi "Match" mangta hai
-    if (waitingUser) {
-        // Agar koi pehle se wait kar raha hai, toh unhe ek room mein daal do
-        const roomId = waitingUser.id + '#' + socket.id;
-        socket.join(roomId);
-        waitingUser.join(roomId);
+    // YEH WALA HISSA ZAROORI HAI: Jab user "START" dabaye
+    socket.on('start-match', () => {
+        if (waitingUser && waitingUser.id !== socket.id) {
+            const roomId = waitingUser.id + '#' + socket.id;
+            socket.join(roomId);
+            waitingUser.join(roomId);
 
-        // Dono ko batao ki partner mil gaya
-        io.to(roomId).emit('matched', roomId);
-        waitingUser = null;
-    } else {
-        // Agar koi nahi hai, toh is user ko wait karao
-        waitingUser = socket;
-    }
+            io.to(roomId).emit('matched', roomId);
+            console.log("Match Found! Room:", roomId);
+            waitingUser = null;
+        } else {
+            waitingUser = socket;
+            console.log("User waiting for match...");
+        }
+    });
 
     socket.on('signal', (data) => {
-        // Signal sirf uske room mein bhejo
+        // Signal ko uske partner tak pahuchao
         const rooms = Array.from(socket.rooms);
         const roomId = rooms.find(r => r.includes('#'));
         if (roomId) {
@@ -38,6 +30,3 @@ io.on('connection', (socket) => {
         if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
     });
 });
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server live on ${PORT}`));
