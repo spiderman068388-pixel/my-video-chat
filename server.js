d968ac29332fd5e4897712b2decddc29dff62ca1
spiderman068388-pixel <spiderman068388@gmail.com>
2026-03-06 15:00:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('./'));
+
+let waitingUser = null;
+
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    // YEH WALA HISSA ZAROORI HAI: Jab user "START" dabaye
+    // Jab user "START" dabaye (index.html se aata hai)
     socket.on('start-match', () => {
         if (waitingUser && waitingUser.id !== socket.id) {
             const roomId = waitingUser.id + '#' + socket.id;
             socket.join(roomId);
             waitingUser.join(roomId);
 
+            // Dono ko batao ki partner mil gaya
             io.to(roomId).emit('matched', roomId);
             console.log("Match Found! Room:", roomId);
             waitingUser = null;
@@ -17,8 +27,9 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Sabse important: Video signals exchange karna
     socket.on('signal', (data) => {
-        // Signal ko uske partner tak pahuchao
+        // Signal ko uske room ke doosre partner tak bhejo
         const rooms = Array.from(socket.rooms);
         const roomId = rooms.find(r => r.includes('#'));
         if (roomId) {
@@ -28,5 +39,9 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
+        console.log('User disconnected');
     });
 });
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => console.log(`Server live hai port ${PORT} par`));
