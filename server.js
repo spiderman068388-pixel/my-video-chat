9397e5d06eeb4711357d0f54457e7ae6e13a24bc
spiderman068388-pixel <spiderman068388@gmail.com>
2026-03-07 20:50:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,29 +8,19 @@ app.use(express.static('./'));
 let waitingUser = null;
 
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-
     socket.on('start-match', () => {
-        // Agar koi pehle se wait kar raha hai aur wo main khud nahi hoon
         if (waitingUser && waitingUser.id !== socket.id) {
-            const roomId = `${waitingUser.id}#${socket.id}`;
-            
+            const roomId = waitingUser.id + '#' + socket.id;
             socket.join(roomId);
             waitingUser.join(roomId);
-
-            // Dono ko roomId bhej rahe hain taaki wo isi room mein signal karein
             io.to(roomId).emit('matched', roomId);
-            
-            console.log("Match Found! Room:", roomId);
-            waitingUser = null; 
+            waitingUser = null;
         } else {
             waitingUser = socket;
-            console.log("User waiting for match...");
         }
     });
 
     socket.on('signal', (data) => {
-        // Sabse zaroori: Signal ko sirf uske partner ko bhejo room ke zariye
         if (data.roomId) {
             socket.to(data.roomId).emit('signal', data);
         }
@@ -38,9 +28,8 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
-        console.log('User disconnected');
     });
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`Server live on port ${PORT}`));
+server.listen(PORT, () => console.log('Server running on port ' + PORT));
