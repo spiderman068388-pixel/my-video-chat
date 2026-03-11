13502fbb2c8ef9c00ff69a07a28682ad9f47f5dd
spiderman068388-pixel <spiderman068388@gmail.com>
2026-03-12 00:57:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path'); // Ye line path setup karti hai
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const path = require('path');
 
-// Sabhi static files (css, js, images) ko load karne ke liye
-app.use(express.static(path.join(__dirname, '/')));
+// Ye line CSS aur JS files ko dhoondhne mein madad karegi
+app.use(express.static(__dirname));
 
-// Sirf index.html file ko serve karne ke liye
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-});
-
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
