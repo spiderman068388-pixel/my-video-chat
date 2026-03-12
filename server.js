3527eed9ba26d013dab882e037f78cfdb6df7ac7
spiderman068388-pixel <spiderman068388@gmail.com>
2026-03-12 22:41:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
-// Static files ko dhoondhne ke liye (style.css, script.js)
-app.use(express.static(__dirname));
+// SABSE ZAROORI LINE: Ye aapki CSS aur JS ko dhoondhne mein madad karegi
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Matching Logic (Abhi ke liye basic setup)
+let waitingUser = null;
+io.on('connection', (socket) => {
+    console.log('User connected: ' + socket.id);
+    
+    socket.on('find-partner', () => {
+        if (waitingUser && waitingUser !== socket.id) {
+            io.to(socket.id).emit('partner-found');
+            io.to(waitingUser).emit('partner-found');
+            waitingUser = null;
+        } else {
+            waitingUser = socket.id;
+        }
+    });
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`App is live on port ${PORT}`);
+http.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
 });
